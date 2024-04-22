2ee1d53da7a28598938a39b6d615c537516cfff7
Nelson Pino <devsoftware.etiquetas@gmail.com>
2024-04-22 13:31:20 -0400

    creacion del aplication dbcontext

---
+++ BackendBlazorSecurity8/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Orders.Shared.Entities;
+using SharedBlazorSecurity.Models;
+
+#pragma warning disable IDE0290 // Usar constructor principal
+namespace BackendBlazorSecurity8.Data
+{
+	public class ApplicationDbContext : IdentityDbContext<User>
+	{
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Category> Categorias { get; set; }
+		public DbSet<City> Ciudades { get; set; }
+		public DbSet<Country> Paises { get; set; }
+		public DbSet<State> Estados { get; set; }
+	}
+}
